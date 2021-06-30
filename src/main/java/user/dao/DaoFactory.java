fd6810f65a9343f9d7b4968d843efa9bd4e05984
김성규 <kskyu610@gmail.com>
2021-06-30 17:36:16 +0900

    refactor: DaoFactor 클래스 - 유지보수를 위해 ConnectionMaker 생성을 메서드로 추출

--- src/main/java/user/dao/DaoFactory.java
+++ src/main/java/user/dao/DaoFactory.java
@@ -3,6 +3,10 @@ package user.dao;
 public final class DaoFactory {
 
     public final UserDao userDao() {
-        return new UserDao(new DConnectionMaker());
+        return new UserDao(connectionMaker());
+    }
+
+    public final ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
