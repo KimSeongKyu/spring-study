a94e4dbaca508a4acde4b18d09b8e4a145be4410
김성규 <kskyu610@gmail.com>
2021-06-30 12:12:54 +0900

    feat: UserDao 클래스 - 테스트를 위한 main 기능 구현

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -6,8 +6,24 @@ import java.sql.*;
 
 public final class UserDao {
 
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao userDao = new UserDao();
+
+        User user = new User("seongkyu", "김성규", "developer");
+
+        userDao.addUser(user);
+
+        System.out.println(user.id() + " 등록 성공");
+
+        User user2 = userDao.getUser(user.id());
+        System.out.println(user2.name());
+        System.out.println(user2.password());
+
+        System.out.println(user2.id() + " 조회 성공");
+    }
+
     public final void addUser(final User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         final Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost/spring_study", "root", "root");
 
@@ -24,7 +40,7 @@ public final class UserDao {
     }
 
     public final User getUser(final String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         final Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost/spring_study", "root", "root");
 
