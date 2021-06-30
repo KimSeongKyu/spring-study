9c81f53cb9c7c0c3802ff087ef09bf736197e6a6
김성규 <kskyu610@gmail.com>
2021-06-30 17:09:47 +0900

    feat: SimpleConnectionMaker 클래스 - DB 커넥션 연결 기능 구현

--- src/main/java/user/dao/SimpleConnectionMaker.java
+++ src/main/java/user/dao/SimpleConnectionMaker.java
@@ -1,4 +1,14 @@
 package user.dao;
 
-public class SimpleConnectionMaker {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class SimpleConnectionMaker {
+
+    public final Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost/spring_study", "root", "root");
+    }
 }
