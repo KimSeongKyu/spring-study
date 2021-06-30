301119a498d5c70bc2efb8f22f754fad4e51c93b
김성규 <kskyu610@gmail.com>
2021-06-30 11:38:16 +0900

    feat: UserDao 클래스 - DB에 사용자를 저장하는 기능 구현

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -1,4 +1,28 @@
 package user.dao;
 
-public class UserDao {
+import user.domain.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public final class UserDao {
+
+    public final void addUser(final User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        final Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost/spring_study", "root", "root");
+
+        final PreparedStatement preparedStatement = connection.prepareStatement(
+                "insert into users(id, name, password) values(?,?,?)");
+        preparedStatement.setString(1, user.id());
+        preparedStatement.setString(2, user.name());
+        preparedStatement.setString(3, user.password());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
 }
