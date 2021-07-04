1eb9959d0033e4b12f98ead6dc3c082cd34b3518
김성규 <kskyu610@gmail.com>
2021-07-04 21:46:14 +0900

    feat: connection을 맺은 횟수를 계산하는 기능 구현

---
+++ src/main/java/user/dao/UserDaoConnectionCountingTest.java
@@ -0,0 +1,36 @@
+package user.dao;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoConnectionCountingTest {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+        User user = new User("seongkyu", "김성규", "developer");
+
+        userDao.addUser(user);
+        System.out.println(user.id() + " 등록 성공");
+
+        User user2 = userDao.getUser(user.id());
+        System.out.println(user2.name());
+        System.out.println(user2.password());
+        System.out.println(user2.id() + " 조회 성공");
+
+        User user3 = userDao.getUser(user.id());
+        System.out.println(user3.name());
+        System.out.println(user3.password());
+        System.out.println(user3.id() + " 조회 성공");
+
+        User user4 = userDao.getUser(user.id());
+        System.out.println(user4.name());
+        System.out.println(user4.password());
+        System.out.println(user4.id() + " 조회 성공");
+
+        CountingConnectionMaker countingConnectionMaker = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println("Connection counter : " + countingConnectionMaker.counter());
+    }
+}
