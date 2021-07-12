55754c32dd3a718b17b673927f1eafff787e140e
김성규 <kskyu610@gmail.com>
2021-07-12 23:15:23 +0900

    test: UserDao 테스트에 JUnit 프레임워크 적용

---
+++ src/test/java/user/dao/UserDaoTest.java
@@ -0,0 +1,26 @@
+package user.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserDaoTest {
+
+    @Test
+    public void addAndGetUserTest() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+        User addedUser = new User("seongkyu", "김성규", "developer");
+
+        userDao.addUser(addedUser);
+        User gotUser = userDao.getUser(addedUser.id());
+
+        assertThat(gotUser.name()).isEqualTo(addedUser.name());
+        assertThat(gotUser.password()).isEqualTo(addedUser.password());
+    }
+}
