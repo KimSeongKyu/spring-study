11a89fc4dfcf3f7905573f353e6326ac85639682
김성규 <kskyu610@gmail.com>
2021-07-12 23:32:15 +0900

    test: Users 테이블 레코드 수를 반환하는 단위 테스트 작성

--- src/test/java/user/dao/UserDaoTest.java
+++ src/test/java/user/dao/UserDaoTest.java
@@ -29,4 +29,26 @@ class UserDaoTest {
         assertThat(gotUser.name()).isEqualTo(addedUser.name());
         assertThat(gotUser.password()).isEqualTo(addedUser.password());
     }
+
+    @Test
+    public void getCountTest() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+
+        User addedUser1 = new User("seongkyu1", "김성규", "developer");
+        User addedUser2 = new User("seongkyu2", "김성규", "developer");
+        User addedUser3 = new User("seongkyu3", "김성규", "developer");
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
+
+        userDao.addUser(addedUser1);
+        assertThat(userDao.getCount()).isEqualTo(1);
+
+        userDao.addUser(addedUser2);
+        assertThat(userDao.getCount()).isEqualTo(2);
+
+        userDao.addUser(addedUser3);
+        assertThat(userDao.getCount()).isEqualTo(3);
+    }
 }
